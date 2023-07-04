cad092f46c45a12b6a194d4366a46db460041b9f
stephdinu <stephaniedinu@gmail.com>
2023-07-04 16:55:54 +0200

    problem 5

---
+++ src/src/PalindromeCheck.java
@@ -0,0 +1,30 @@
+public class PalindromeCheck {
+    public String longestPalindrome(String s) {
+        char[] charArray = s.toCharArray();
+        int start = 0;
+        int maxLen = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            for (int len = 0; i + len < charArray.length; len++) {
+                if (isPalindrome(charArray, i, len) && len + 1 > maxLen) {
+                    maxLen = len + 1;
+                    start = i;
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+
+    public boolean isPalindrome(char[] charArray, int start, int len) {
+        while (len > 0) {
+            if (charArray[start] != charArray[start + len]) {
+                return false;
+            }
+
+            start++;
+            len -= 2;
+        }
+
+        return true;
+    }
+}
