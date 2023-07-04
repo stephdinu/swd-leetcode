22d3c2b7172dfd298efe71cb20bd110889d1e4a3
stephdinu <stephaniedinu@gmail.com>
2023-07-04 16:53:57 +0200

    problem 3

---
+++ src/src/SubStringCheck.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SubStringCheck {
+    public int lengthOfLongestSubstring(String s) {
+        int i = 0;
+        int j = 0;
+        int max = 0;
+        Set<Character> set = new HashSet<>();
+
+        while (j < s.length()) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                max = Math.max(max, set.size());
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return max;
+    }
+}
